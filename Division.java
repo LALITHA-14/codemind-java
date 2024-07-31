24315de334546e7d8eb30b785a272ecdc74debe0
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-07-31 15:39:38 +0530

    import java.util.Scanner; public class example{     public static void main(String[] args){         Scanner div=new Scanner(System.in);         int a,b,c;         a=div.nextInt();         b=div.nextInt();         c=a/b;         System.out.println(c);     } }

---
+++ Division.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class example{
+    public static void main(String[] args){
+        Scanner div=new Scanner(System.in);
+        int a,b,c;
+        a=div.nextInt();
+        b=div.nextInt();
+        c=a/b;
+        System.out.println(c);
+    }
+}
