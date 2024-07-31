1fa24dcba52679cb80bae5fd5b811ffc993dab22
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-07-31 15:44:40 +0530

    import java.util.Scanner; public class Example{     public static void main(String[] args){         int a,b,c;         Scanner multiplication=new Scanner(System.in);         a=multiplication.nextInt();         b=multiplication.nextInt();         c=a*b;         System.out.println(c);     } }

---
+++ Multiplication.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Example{
+    public static void main(String[] args){
+        int a,b,c;
+        Scanner multiplication=new Scanner(System.in);
+        a=multiplication.nextInt();
+        b=multiplication.nextInt();
+        c=a*b;
+        System.out.println(c);
+    }
+}
