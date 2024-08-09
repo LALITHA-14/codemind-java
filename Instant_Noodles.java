ac47529dbafc438ee0ede9fc867ea2e919681cbc
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-08-09 22:09:33 +0530

    import java.util.Scanner; public class noodles{     public static void main(String[] args){         Scanner min=new Scanner(System.in);         int a=min.nextInt();         int b=min.nextInt();         int pro=a*b;         System.out.printf("%d",pro);     } }

---
+++ Instant_Noodles.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class noodles{
+    public static void main(String[] args){
+        Scanner min=new Scanner(System.in);
+        int a=min.nextInt();
+        int b=min.nextInt();
+        int pro=a*b;
+        System.out.printf("%d",pro);
+    }
+}
