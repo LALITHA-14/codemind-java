372a8a6629b8c01ce09be577fccd8ea6658453fd
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-08-19 22:25:20 +0530

    Integer_Literals.java

---
+++ Integer_Literals.java
@@ -0,0 +1,13 @@
+// Different types of Integer Literals
+public class IntegerLiterals {
+    public static void main(String[] args) {
+        int a=10;
+        int b=0b10111;
+        int c=0123;
+        int d=0xF;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
