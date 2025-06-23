7387d54aef86d1735c074cc9ae76c5c37470ab7b
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2025-06-23 21:15:11 +0530

    import java.util.Scanner;

---
+++ ScannerMethods.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// update your code below this line
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		System.out.print(num*num);
+	}
+}
