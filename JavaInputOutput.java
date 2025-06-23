6ea715e9279e7a9b30349a48bbd39bac94577253
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2025-06-23 22:12:41 +0530

    class Solution {

---
+++ JavaInputOutput.java
@@ -0,0 +1,8 @@
+// User function Template for Java
+class Solution {
+    static void printIntString(String S, int N) {
+        // code here
+        System.out.printf("The input string :%s\n",S);
+        System.out.printf("The input integer :%d\n",N);
+    }
+}
