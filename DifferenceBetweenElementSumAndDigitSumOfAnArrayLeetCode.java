c32a664c5dae91136fa72624a9449a2fc584f417
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2025-07-01 21:12:29 +0530

    class Solution {

---
+++ DifferenceBetweenElementSumAndDigitSumOfAnArrayLeetCode.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int differenceOfSum(int[] nums) {
+        int totalsum=0;
+        int digitsum=0;
+        for(int num:nums){
+            totalsum+=num;
+            while(num>0){
+                digitsum+=num%10;
+                num=num/10;
+            }
+        }
+        return totalsum-digitsum;
+    }
+}
